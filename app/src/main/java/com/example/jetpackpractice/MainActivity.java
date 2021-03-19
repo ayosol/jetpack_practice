526f4b6f184ef73265ac19fd0b80207d0ee2153d
Solomon-Ayodele-O <solomonayodeleo@gmail.com>
2021-03-19 20:57:30 +0100

    Bottom Navigation View example

---
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.jetpackpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpNavigation();
+
+    }
+
+    public void setUpNavigation() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        /*NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());*/
+    }
+}   
